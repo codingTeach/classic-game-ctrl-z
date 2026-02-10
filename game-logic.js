1da7f4e36b8f86ce3ce1524e73faea6ee4017ac0
Marco Gabriel Perez Montiel <marcoperez119pm@gmail.com>
2026-02-10 17:43:27 -0600

    Nave de mueve solo en X

--- game-logic.js
+++ game-logic.js
@@ -3,14 +3,15 @@ const player = document.querySelector('#player');
 const speed = 0.15;
 
 const limitX = 15;
-const minZ = -5;
-const maxZ = 0;
+//const minZ = -5;
+//const maxZ = 0;
 
 const keys = {};
 
 window.addEventListener('keydown', e => keys[e.key]= true);
 window.addEventListener('keyup', e => keys[e.key] = false);
 
+//quitar los // si quieren que la nave se mueva en Z
 function movePlayer(){
     const pos = player.object3D.position;
 
@@ -20,12 +21,12 @@ function movePlayer(){
     if (keys['ArrowRight'] || keys['d']){
         if(pos.x < limitX) pos.x += speed;
     }
-    if(keys['Arrowup'] || keys['w']){
-        if(pos.z > minZ) pos.z -= speed;
-    }
-    if(keys['Arrowdown'] || keys['s']){
-        if(pos.z < maxZ) pos.z += speed;
-    }
+    //if(keys['Arrowup'] || keys['w']){
+       // if(pos.z > minZ) pos.z -= speed;
+    //}
+    //if(keys['Arrowdown'] || keys['s']){
+       // if(pos.z < maxZ) pos.z += speed;
+   // }
 
     requestAnimationFrame(movePlayer);
 }
