2427a422e5fb7485cea3ef4b385f14db5464cc6d
Marco Gabriel Perez Montiel <marcoperez119pm@gmail.com>
2026-02-11 17:11:04 -0600

    Disparo v1

--- game-logic.js
+++ game-logic.js
@@ -10,6 +10,7 @@ const keys = {};
 
 window.addEventListener('keydown', e => keys[e.key]= true);
 window.addEventListener('keyup', e => keys[e.key] = false);
+window.addEventListener('keydown', e => {if(e.code === 'Space'){shoot();}});
 
 //quitar los // si quieren que la nave se mueva en Z
 function movePlayer(){
@@ -32,3 +33,44 @@ function movePlayer(){
 }
 
 movePlayer();
+
+
+function shoot() {
+    console.log = ("bala");
+    const player = document.querySelector('#player');
+    const scene = document.querySelector('a-scene');
+
+
+    const bala = document.createElement('a-sphere');
+
+    const playerPos = player.object3D.position;
+
+    bala.setAttribute('position', {
+        x: playerPos.x,
+        y: 0.5,
+        z: playerPos.z + 0.5});
+
+
+
+    bala.setAttribute('radius', 0.1);
+    bala.setAttribute('color', 'yellow');
+
+    scene.appendChild(bala);
+
+    movimientobala(bala);
+}
+
+function movimientobala(bala){
+    const speed = 0.2;
+
+    function update(){
+        bala.object3D.position.z -= speed;
+
+        if (bala.object3D.position.z < -30){
+            bala.parentNode.removeChild(bala);
+            return;
+        }
+        requestAnimationFrame(update);
+    }
+    update();
+}
